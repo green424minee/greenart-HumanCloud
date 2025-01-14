6064d663e74fa355510fb466d52da4b3e51ee06a
RI4RU <dlw0603@gmail.com>
2025-01-14 11:43:05 +0900

    전체 공고문 리스트를 볼 수 있도록 구현
    공고문 리스트 전체(업데이트 내림차순)를 확인하고
    공고문 리스트를 조건에 따라 검색할 수 있도록 구현
    키워드는 공고문의 제목과 회사 이름을 검색함
    jsp가 하드코딩 되어있기 때문에 추후에 리팩토링을 통해 선택한 옵션을 검색후에도 유지할 수 있도록 할 필요가 있음

---
+++ src/main/java/ko/co/green/recruitment/RecruitmentList.java
@@ -0,0 +1,26 @@
+package ko.co.green.recruitment;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RecruitmentList {
+	private String name;
+	private String contact;
+	private String email;
+	private String website;
+	private String address;
+	private String industry;
+	private String title;
+	private String description;
+	private LocalDate end_date;
+	private LocalDateTime updated_at;
+}
