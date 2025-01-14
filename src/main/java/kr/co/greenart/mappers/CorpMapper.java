ec079e3bae5dfa8ecd6b2db36a590b5af91d4dce
hj-rxl <rubyxluna@gmail.com>
2025-01-14 16:28:20 +0900

    CorpMapper 주석 복원

--- src/main/java/kr/co/greenart/mappers/CorpMapper.java
+++ src/main/java/kr/co/greenart/mappers/CorpMapper.java
@@ -12,23 +12,23 @@ import kr.co.green.corp.Corp;
 
 public interface CorpMapper {
     @Select("SELECT * FROM company")
-//    @Results(id = "corpResults", value = {
-//            @Result(column = "id", property = "id", id = true)
-//            , @Result(column = "corpid", property = "corpid")
-//            , @Result(column = "password", property = "password")
-//            , @Result(column = "business_reg_no", property = "businessRegNo")
-//            , @Result(column = "image", property = "image")
-//            , @Result(column = "name", property = "name")
-//            , @Result(column = "description", property = "description")
-//            , @Result(column = "contact", property = "contact")
-//            , @Result(column = "email", property = "email")
-//            , @Result(column = "owner", property = "owner")
-//            , @Result(column = "est_date", property = "estDate")
-//            , @Result(column = "website", property = "website")
-//            , @Result(column = "emp_count", property = "empCount")
-//            , @Result(column = "sales", property = "sales")
-//            , @Result(column = "address", property = "address")
-//    })
+    @Results(id = "corpResults", value = {
+            @Result(column = "id", property = "id", id = true)
+            , @Result(column = "corpid", property = "corpid")
+            , @Result(column = "password", property = "password")
+            , @Result(column = "business_reg_no", property = "businessRegNo")
+            , @Result(column = "image", property = "image")
+            , @Result(column = "name", property = "name")
+            , @Result(column = "description", property = "description")
+            , @Result(column = "contact", property = "contact")
+            , @Result(column = "email", property = "email")
+            , @Result(column = "owner", property = "owner")
+            , @Result(column = "est_date", property = "estDate")
+            , @Result(column = "website", property = "website")
+            , @Result(column = "emp_count", property = "empCount")
+            , @Result(column = "sales", property = "sales")
+            , @Result(column = "address", property = "address")
+    })
     List<Corp> selectAll();
     
     @Insert("INSERT INTO company (business_reg_no, corpid, password, name, contact, email, owner, address) "
@@ -36,11 +36,11 @@ public interface CorpMapper {
     int insertCorp(Corp corp);
     
     @Select("SELECT * FROM company WHERE corpid = #{corpId}")
-//    @Results(id = "corpResult", value = {
-//            @Result(column = "id", property = "id", id = true)
-//            , @Result(column = "corpid", property = "corpid")
-//            , @Result(column = "password", property = "password")
-//    })
+    @Results(id = "corpResult", value = {
+            @Result(column = "id", property = "id", id = true)
+            , @Result(column = "corpid", property = "corpid")
+            , @Result(column = "password", property = "password")
+    })
     Corp selectById(String corpId);
     
     @Update("UPDATE company SET " +
