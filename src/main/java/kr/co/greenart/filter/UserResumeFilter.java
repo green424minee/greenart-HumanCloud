341ace8412c2fa2d8172f388af4668cc45616867
RI4RU <dlw0603@gmail.com>
2025-01-15 15:32:56 +0900

    필터 구성에 따른 web.xml 파일 수정 + 필터 생성
    마이페이지를 볼때 로그인 여부를 확인
    이력서의 개별문서를 확인할 때 자신의 이력서인지 확인하는 필터
    남의 이력서에 접근하면 응답코드400 발령

--- src/main/java/kr/co/greenart/filter/UserResumeFilter.java
+++ src/main/java/kr/co/greenart/filter/UserResumeFilter.java
@@ -11,21 +11,31 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import kr.co.greenart.resume.Resume;
+import kr.co.greenart.resume.ResumeService;
+import kr.co.greenart.user.User;
 
-@WebFilter(filterName = "UserMyPageFilter", urlPatterns = {"/HumanCloud/user/MyPage/*"})
-public class UserMyPageFilter implements Filter {
+@WebFilter(filterName = "UserResumeFilter", urlPatterns = "/HumaCloud/user/MyPage/resume/my/*")
+public class UserResumeFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		ResumeService service = ResumeService.getInstance();
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		
 		HttpSession session = req.getSession(false);
 		
-		if (session == null || session.getAttribute("login") == null) {
-			resp.sendRedirect("/_greenart_HumanCloud/HumanCloud/user/Signin");
-		} else {
+		String id = req.getParameter("id");
+		int resume_id = Integer.parseInt(id);
+		Resume resume = service.selectMyResume(resume_id);
+		int user_id = resume.getUser_id();
+		User user = (User) session.getAttribute("login");
+		
+		if (user_id == user.getId()) {
 			chain.doFilter(request, response);
+		} else {
+			resp.sendError(400, "Invalid action");
 		}
 	}
 }
