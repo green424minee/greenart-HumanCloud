800a733746f7aa3a4dd0916fbcac6e2696073fb3
RI4RU <dlw0603@gmail.com>
2025-01-20 12:49:50 +0900

    매핑 충돌오류 수정

--- src/main/java/kr/co/green/corp/JobOfferSerlvet.java
+++ src/main/java/kr/co/green/corp/JobOfferSerlvet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/HumanCloud/corp/SearchApplicantsServlet")
+@WebServlet("/HumanCloud/corp/SearchApplicantsServlet/JobOffer")
 public class JobOfferSerlvet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
